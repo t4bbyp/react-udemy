7e4298d2a9ffb0111af149908151b8b897e6e8b4
t4bbyp <tvbby.p@gmail.com>
2026-04-18 10:06:45 +0300

    start of section 21

---
+++ 21-adv-redux/src/App.js
@@ -0,0 +1,20 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
+
+/*
+NOTES: 
+- reducers must me pure, side-effect free, synchronous functions
+
+*/
