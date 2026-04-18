0ec756f9c10622bb9b3c19947b35152bfaaebb7f
t4bbyp <tvbby.p@gmail.com>
2026-04-18 17:13:25 +0300

    finished setting up the project

--- 21-adv-redux/src/App.js
+++ 21-adv-redux/src/App.js
@@ -1,11 +1,14 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
@@ -16,5 +19,6 @@ export default App;
 /*
 NOTES: 
 - reducers must me pure, side-effect free, synchronous functions
-
+- side-effects & async tasks can go: in the components (useEffect()) or
+inside custom action creators
 */
--- 21-adv-redux/src/components/Cart/CartButton.js
+++ 21-adv-redux/src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+  const toggleCart = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCart}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
